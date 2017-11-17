d8a2673d3db81fc90a6e069b0e268d5bfb4ae944
lvergne1 <lvergne1@ccp.edu>
2017-11-16 21:24:57 -0500

    Add files via upload

--- VergnettiColCon/src/vergnetticolcon/VergnettiColCon.java
+++ VergnettiColCon/src/vergnetticolcon/VergnettiColCon.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vergnetticolcon;
 import java.math.*;
 /**
@@ -11,21 +7,62 @@ import java.math.*;
  */
 public class VergnettiColCon {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-            
-      for (int i = 1; i < 20000; i++){
-          if (i%2!=0){
-              for (int n = 1; n < 25; n++){
-                if (i >= Math.pow(2, n) && i < Math.pow(2, n+1)){
-                System.out.println("int: " + i + " power: " + n  + "Theta: " + (i - Math.pow(2,n))+"PI/" + Math.pow(2,n-1));
-                
-                }     
-              }   
-          }
-      }
+        int[] angle = new int[2];
+              Transform(129,0);
+
+
     }
     
+    public static int[] toAngle(int i){
+        int[] angle = new int[2]; 
+        for (int n = 1; n < 25; n++){
+            if (i >= Math.pow(2, n) && i < Math.pow(2, n+1)){
+                angle[0] = (int) (i-Math.pow(2,n));
+                angle[1] = (int) (Math.pow(2, n-1));
+                    
+                break;
+            }
+        }    
+        reduce(angle);
+        return angle;
+    }
+    
+    public static int[] subtract(int i, int j){
+        
+        int s;
+        if (i - j < 0){
+            s = j-i;
+        }
+        else{
+            s = i-j;
+        }
+        int[] angle = toAngle(s);
+        return angle; 
+    }
+
+    public static void reduce(int[] angle){
+        while ((angle[0]%2==0)&& (angle[1]%2==0)){
+            angle[0] = angle[0]/2;
+            angle[1] = angle[1]/2;
+        }
+    }
+    
+    public static void printAngle(int[] angle){
+          System.out.println(angle[0]+"π/"+angle[1]);
+    }
+    
+    public static void Transform(int i, int j){
+        System.out.println("Angle before Reduction for n="+i);
+        printAngle(toAngle(i));
+       // printAngle(subtract(i,j));
+        while (i%2==0){
+            i = i/2;
+            System.out.println("run");
+        }
+        if (i != 1){
+            System.out.println("runAtBase");
+            Transform((3*i)+1, i);
+        }
+    }
 }
